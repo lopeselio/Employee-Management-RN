b8c81d3ec84b0c83fac8d2faef18065e924a39fd
lopeselio <lopeselio@gmail.com>
2020-12-09 10:10:49 +0530

    added Home

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import Constants from 'expo-constants'
+import Home from './screens/Home'
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Home />
     </View>
   );
 }
@@ -15,7 +16,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight
   },
 });
---
+++ screens/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+
+function Home(){
+    return (
+        <Text>Hello from Home JS</Text>
+    )
+}
+
+export default Home
